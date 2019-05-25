ea91332d986c61ee931bc75fbd8e0c4e1166b15e
Aaron Bush <asb.bush@gmail.com>
2019-05-24 21:58:06 -0400

    init commit of drawing using raylib

---
+++ cursled/cmd/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	"github.com/gookit/color"
+)
+
+const StartSentinel uint32 = 0xDEADBEEF
+
+type Header struct {
+	NumLEDs uint16
+}
+
+type ScreenKey struct {
+	Row    uint8
+	Column uint8
+}
+
+type LEDInfo struct {
+	Row        uint8
+	Column     uint8
+	Red        uint8
+	Green      uint8
+	Blue       uint8
+	Brightness uint8
+}
+
+func main() {
+	maxLeds := uint16(1000)
+	maxRows := uint8(40)
+	numRows := uint8(0)
+	maxColumns := uint8(20)
+	numColumns := uint8(0)
+
+	// i, _ := os.Open(os.Args[1])
+	// defer i.Close()
+
+	b := makeLedsData()
+	i := bytes.NewReader(b)
+
+	ready := false
+	dataIn := make([]byte, 4)
+
+	for {
+		if !ready {
+			_, err := i.Read(dataIn)
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				log.Fatal("error reading for start sentinel", err)
+			}
+			if binary.BigEndian.Uint32(dataIn) == StartSentinel {
+				ready = true
+			}
+		}
+
+		// we are ready...
+		header := Header{}
+
+		if err := binary.Read(i, binary.BigEndian, &header); err != nil {
+			log.Fatal("error reading in the header", err)
+			return
+		}
+		header.NumLEDs = min(header.NumLEDs, maxLeds)
+
+		ledInfo := LEDInfo{}
+		log.Printf("Reading in info for %d LEDs\n", header.NumLEDs)
+
+		ledInfos := make(map[ScreenKey]LEDInfo)
+
+		for n := uint16(0); n < header.NumLEDs; n++ {
+			log.Println("reading in info for led #", n)
+			if err := binary.Read(i, binary.BigEndian, &ledInfo); err != nil {
+				log.Fatal("error reading in the led info", err)
+				return
+			}
+			if ledInfo.Column > maxColumns || ledInfo.Row > maxRows {
+				continue // discard out of range leds
+			}
+			ledInfos[ScreenKey{ledInfo.Row, ledInfo.Column}] = ledInfo
+
+			numColumns = max8(ledInfo.Column, numColumns)
+			numRows = max8(ledInfo.Row, numRows)
+		}
+		numColumns = min8(numColumns, maxColumns)
+		numRows = min8(numRows, maxRows)
+		drawTable(ledInfos, numRows, numColumns)
+		ready = false
+	}
+}
+
+func drawTable(leds map[ScreenKey]LEDInfo, numRows uint8, numColumns uint8) {
+	log.Printf("%dx%d -> %v\n", numRows, numColumns, leds)
+	black := color.BgBlack.Sprint("  ")
+	for row := uint8(1); row <= numRows; row++ {
+		fmt.Printf("%02d:", row)
+		var sb strings.Builder
+		for column := uint8(1); column <= numColumns; column++ {
+			if led, ok := leds[ScreenKey{row, column}]; ok {
+				c := color.RGB(led.Red, led.Green, led.Blue, true)
+				sb.WriteString(c.Sprintf("  "))
+			} else {
+				sb.WriteString(black)
+			}
+			fmt.Printf(sb.String())
+			sb.Reset()
+		}
+		fmt.Printf(":%02d\n", row)
+	}
+	// for _, led := range leds {
+	// 	fmt.Println(led)
+	// 	var sb strings.Builder
+	// 	c := color.RGB(led.Red, led.Blue, led.Green, true)
+	// 	sb.WriteString(c.Sprintf("  "))
+	// 	fmt.Println(sb.String())
+	// }
+}
+
+func max(x, y uint16) uint16 {
+	if x > y {
+		return x
+	}
+	return y
+}
+func max8(x, y uint8) uint8 {
+	return uint8(max(uint16(x), uint16(y)))
+}
+
+func min(x, y uint16) uint16 {
+	if max(x, y) == x {
+		return y
+	}
+	return x
+}
+
+func min8(x, y uint8) uint8 {
+	return uint8(min(uint16(x), uint16(y)))
+}
+
+func filter(leds []LEDInfo, test func(LEDInfo) bool) (ret []LEDInfo) {
+	for _, l := range leds {
+		if test(l) {
+			ret = append(ret, l)
+		}
+	}
+	return
+}
+
+func makeLedsData() []byte {
+	return []byte{0xDE, 0xAD, 0xBE, 0xEF, // start sentinel
+		0x00, 0x06, // numLEDs
+
+		0x01, 0x01, // row, column
+		0xff, 0x00, 0x00, // RGB
+		0xFF, // Brightness
+
+		0x01, 0x02, // row, column
+		0x00, 0xff, 0xff, // RGB
+		0xFF, // Brightness
+
+		0x01, 0x03, // row, column
+		0xff, 0xff, 0xff, // RGB
+		0xFF, // Brightness
+
+		// row 2
+		0x02, 0x01, // row, column
+		0xff, 0x00, 0x00, // RGB
+		0xFF, // Brightness
+
+		0x02, 0x02, // row, column
+		0x00, 0xff, 0xff, // RGB
+		0xFF, // Brightness
+
+		0x02, 0x03, // row, column
+		0xff, 0xff, 0xff, // RGB
+		0xFF, // Brightness
+	}
+}
---
+++ cursled/cmd/ui/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type SquareInfo struct {
+	GridCord  GridCord
+	Vector2   rl.Vector2
+	CreatedAt time.Time
+	Color     rl.Color
+}
+
+type GridCord struct {
+	Row    int32
+	Column int32
+}
+
+var gridRows = int32(40)
+var gridColumns = int32(20)
+var gridSpacing = int32(20)
+var gridSpacingFloat = float32(gridSpacing)
+var windowHeight = gridRows * gridSpacing
+var windowWidth = gridColumns * gridSpacing
+var decayTime = 3 * time.Second
+
+func main() {
+	windowHeight := gridRows * gridSpacing
+	windowWidth := gridColumns * gridSpacing
+	traceContents := make(map[GridCord]SquareInfo)
+	drawnContents := make(map[GridCord]SquareInfo)
+
+	rl.InitWindow(windowWidth, windowHeight, "pixel drawing")
+
+	rl.SetTargetFPS(30)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("woohoo", 190, 200, 20, rl.Red)
+		drawGrid()
+		drawSquares(traceContents)
+		drawSquares(drawnContents)
+
+		mousePos := rl.GetMousePosition()
+		squareInfo, err := squareFromCoord(mousePos)
+		if err == nil {
+			fmt.Println(mousePos, " -> ", squareInfo)
+			if rl.IsMouseButtonDown(rl.MouseLeftButton) {
+				squareInfo.Color = rl.Red
+				drawnContents[squareInfo.GridCord] = squareInfo
+			} else {
+				squareInfo.Color = rl.Blue
+				traceContents[squareInfo.GridCord] = squareInfo
+			}
+		}
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
+
+func drawGrid() {
+	for r := int32(0); r <= windowHeight; r += gridSpacing {
+		rl.DrawLine(0, r, windowWidth, r, rl.Black)
+	}
+	for c := int32(0); c <= windowWidth; c += gridSpacing {
+		rl.DrawLine(c, 0, c, windowHeight, rl.Black)
+	}
+}
+
+func drawSquares(squareContets map[GridCord]SquareInfo) {
+	for cord, square := range squareContets {
+		if timeLeft := time.Now().Sub(square.CreatedAt); timeLeft < decayTime {
+			// scale for alpha
+			alpha := 1.0 - float32(timeLeft.Nanoseconds())/float32(decayTime.Nanoseconds())
+			rl.DrawRectangleV(square.Vector2, rl.NewVector2(gridSpacingFloat, gridSpacingFloat), rl.Fade(square.Color, alpha))
+		} else {
+			delete(squareContets, cord)
+		}
+	}
+}
+
+func squareFromCoord(vec rl.Vector2) (SquareInfo, error) { // return top left of square
+	x := int32(vec.X)
+	y := int32(vec.Y)
+
+	if x <= 0 || y <= 0 ||
+		x >= windowWidth || y >= windowHeight {
+		return SquareInfo{}, errors.New("Outside of screen bounds")
+	}
+	xPos := x / gridSpacing
+	yPos := y / gridSpacing
+
+	info := SquareInfo{
+		GridCord:  GridCord{xPos, yPos},
+		Vector2:   rl.NewVector2(float32(xPos*gridSpacing), float32(yPos*gridSpacing)),
+		CreatedAt: time.Now(),
+	}
+	return info, nil
+}
