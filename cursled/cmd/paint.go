8edc957c668404a185cbc169a8aedec364eb42b2
Aaron Bush <asb.bush@gmail.com>
2019-05-30 22:03:46 -0400

    experimenting with cobra

--- cursled/cmd/paint.go
+++ cursled/cmd/paint.go
@@ -1,18 +1,31 @@
-package main
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
 
 import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"flag"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/aaronbush/go-stuff/cursled/frame"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
+
+	"github.com/spf13/cobra"
 )
 
 type SquareInfo struct {
@@ -27,34 +40,55 @@ type GridCord struct {
 	Column uint8
 }
 
-var maxBrightness float32
-var gridRows int32
-var gridColumns int32
-var gridSpacing int32
-var gridSpacingFloat float32
-var windowHeight int32
-var windowWidth int32
-var decayTime time.Duration // = 3 * time.Second
+var (
+	numRows       int32
+	numColumns    int32
+	spacing       int32
+	spacingFloat  float32
+	maxBrightness float32
+	decayTime     time.Duration
+	windowHeight  int32
+	windowWidth   int32
+)
 
-func main() {
-	maxBrightness = float32(flag.Int("brightness", 50, "maximum brightness"))
-	gridColumns = int32(*flag.Int("columns", 20, "columns in grid"))
-	gridRows = int32(*flag.Int("rows", 40, "rows in grid"))
-	gridSpacing = int32(*flag.Int("spacing", 20, "grid spacing"))
-	decayTime = *flag.Duration("delay", 3*time.Second, "fade duration")
-	gridSpacingFloat = float32(gridSpacing)
-	flag.Parse()
+// paintCmd represents the paint command
+var paintCmd = &cobra.Command{
+	Use:   "paint",
+	Short: "Start the paint UI",
+	Long:  `Start the paint-like UI which allows drawing directly on the LED display`,
+	RunE:  paint,
+}
 
-	windowHeight = gridRows * gridSpacing
-	windowWidth = gridColumns * gridSpacing
+func init() {
+	rootCmd.AddCommand(paintCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// paintCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// paintCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	paintCmd.Flags().Int32VarP(&numRows, "rows", "r", 40, "number of rows")
+	paintCmd.Flags().Int32VarP(&numColumns, "columns", "c", 20, "number of columns")
+	paintCmd.Flags().Int32VarP(&spacing, "spacing", "s", 20, "cell spacing")
+	paintCmd.Flags().Float32VarP(&maxBrightness, "brightness", "b", 50, "max brightness")
+	paintCmd.Flags().DurationVarP(&decayTime, "decayTime", "t", 3*time.Second, "decay time (seconds)")
+}
+
+func paint(cmd *cobra.Command, args []string) error {
+	windowHeight = numRows * spacing
+	windowWidth = numColumns * spacing
 	traceContents := make(map[GridCord]SquareInfo)
 	drawnContents := make(map[GridCord]SquareInfo)
+	spacingFloat = float32(spacing)
 
 	trackMouse := false
 	fadeMode := false
 	logMode := true
 
-	fmt.Printf("r:%d, c:%d, w:%d x %d\n", gridRows, gridColumns, windowWidth, windowHeight)
 	// Open a new file for writing only
 	file, err := os.OpenFile(
 		"test.data",
@@ -114,13 +148,14 @@ func main() {
 	}
 
 	rl.CloseWindow()
+	return nil
 }
 
 func drawGrid() {
-	for r := int32(0); r <= windowHeight; r += gridSpacing {
+	for r := int32(0); r <= windowHeight; r += spacing {
 		rl.DrawLine(0, r, windowWidth, r, rl.Black)
 	}
-	for c := int32(0); c <= windowWidth; c += gridSpacing {
+	for c := int32(0); c <= windowWidth; c += spacing {
 		rl.DrawLine(c, 0, c, windowHeight, rl.Black)
 	}
 }
@@ -133,7 +168,7 @@ func drawSquares(squareContets map[GridCord]SquareInfo, fadeMode bool) {
 			if fadeMode {
 				alpha = 1.0 - float32(timeLeft.Nanoseconds())/float32(decayTime.Nanoseconds())
 			}
-			rl.DrawRectangleV(square.Vector2, rl.NewVector2(gridSpacingFloat, gridSpacingFloat), rl.Fade(square.Color, alpha))
+			rl.DrawRectangleV(square.Vector2, rl.NewVector2(spacingFloat, spacingFloat), rl.Fade(square.Color, alpha))
 		} else {
 			delete(squareContets, cord)
 		}
@@ -176,12 +211,12 @@ func squareFromCoord(vec rl.Vector2) (SquareInfo, error) { // return top left of
 		x >= windowWidth || y >= windowHeight {
 		return SquareInfo{}, errors.New("Outside of screen bounds")
 	}
-	xPos := x / gridSpacing
-	yPos := y / gridSpacing
+	xPos := x / spacing
+	yPos := y / spacing
 
 	info := SquareInfo{
 		GridCord:  GridCord{uint8(xPos), uint8(yPos)},
-		Vector2:   rl.NewVector2(float32(xPos*gridSpacing), float32(yPos*gridSpacing)),
+		Vector2:   rl.NewVector2(float32(xPos*spacing), float32(yPos*spacing)),
 		CreatedAt: time.Now(),
 	}
 	return info, nil
---
+++ cursled/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/aaronbush/go-stuff/cursled/cmd"
+
+func main() {
+	cmd.Execute()
+}
