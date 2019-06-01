47aaeabebfd77441bdc570a7280a2ef389c5a4c2
Aaron Bush <asb.bush@gmail.com>
2019-05-31 21:30:14 -0400

    continuing with cobra command for paint and have added gui controls

--- cursled/cmd/paint.go
+++ cursled/cmd/paint.go
@@ -18,11 +18,14 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/aaronbush/go-stuff/cursled/frame"
+	rg "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 
 	"github.com/spf13/cobra"
@@ -30,7 +33,7 @@ import (
 
 type SquareInfo struct {
 	GridCord  GridCord
-	Vector2   rl.Vector2
+	Origin    rl.Vector2
 	CreatedAt time.Time
 	Color     rl.Color
 }
@@ -40,15 +43,28 @@ type GridCord struct {
 	Column uint8
 }
 
+const (
+	maxRGB = 255
+)
+
 var (
-	numRows       int32
-	numColumns    int32
-	spacing       int32
-	spacingFloat  float32
-	maxBrightness float32
-	decayTime     time.Duration
-	windowHeight  int32
-	windowWidth   int32
+	numRows            int32
+	numColumns         int32
+	spacing            int32
+	spacingFloat       float32
+	maxBrightness      float32
+	decayTime          time.Duration
+	windowHeight       int32
+	windowWidth        int32
+	gridOrigin         rl.Vector2
+	gridHeight         int32
+	gridWidth          int32
+	statusBarOrigin    rl.Vector2
+	rightControlOrigin rl.Vector2
+	stausBarHeight     int32 = 60
+	rightControlWidth  int32 = 60
+	gridColor                = rl.RayWhite
+	decayMode                = false
 )
 
 // paintCmd represents the paint command
@@ -79,11 +95,21 @@ func init() {
 }
 
 func paint(cmd *cobra.Command, args []string) error {
-	windowHeight = numRows * spacing
-	windowWidth = numColumns * spacing
+	gridOrigin = rl.NewVector2(0, 0)
+	gridHeight = numRows * spacing
+	gridWidth = numColumns * spacing
+
+	statusBarOrigin = rl.NewVector2(gridOrigin.X, gridOrigin.Y+float32(gridHeight))
+	rightControlOrigin = rl.NewVector2(gridOrigin.X+float32(gridWidth), gridOrigin.Y)
+
+	windowHeight = gridHeight + stausBarHeight
+	windowWidth = gridWidth + stausBarHeight
+
 	traceContents := make(map[GridCord]SquareInfo)
 	drawnContents := make(map[GridCord]SquareInfo)
 	spacingFloat = float32(spacing)
+
+	redValue, greenValue, blueValue := new(int), new(int), new(int)
 
 	trackMouse := false
 	fadeMode := false
@@ -101,16 +127,24 @@ func paint(cmd *cobra.Command, args []string) error {
 	defer file.Close()
 
 	rl.InitWindow(windowWidth, windowHeight, "pixel drawing")
+	rg.LoadGuiStyle("styles/solarized_light.style")
 
 	rl.SetTargetFPS(30)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
+		rl.ClearBackground(rl.Blank)
 
-		drawGrid()
+		drawColor, decayOrigin := drawColorInputs(rightControlOrigin, redValue, greenValue, blueValue)
+		decayMode, _ = drawDecaySettings(decayOrigin, &decayMode)
+
 		drawSquares(traceContents, fadeMode)
 		drawSquares(drawnContents, fadeMode)
+
+		drawGrid() // after colors are drawn to keep grid lines
+
+		statusText := fmt.Sprintf("fade:%t, track:%t, log:%t\nFPS: %.1f (%.03f)", fadeMode, trackMouse, logMode, rl.GetFPS(), rl.GetFrameTime())
+		rl.DrawText(statusText, int32(statusBarOrigin.X+3), int32(statusBarOrigin.Y), 12, rl.Gray)
 
 		if logMode {
 			exportSquares(file, drawnContents, fadeMode)
@@ -118,15 +152,12 @@ func paint(cmd *cobra.Command, args []string) error {
 
 		if rl.IsKeyPressed(rl.KeyF) {
 			fadeMode = !fadeMode
-			log.Printf("Fade Mode: %t", fadeMode)
 		}
 		if rl.IsKeyPressed(rl.KeyT) {
 			trackMouse = !trackMouse
-			log.Printf("Track Mode: %t", trackMouse)
 		}
 		if rl.IsKeyPressed(rl.KeyL) {
 			logMode = !logMode
-			log.Printf("Log Mode: %t", logMode)
 		}
 
 		if trackMouse {
@@ -134,16 +165,16 @@ func paint(cmd *cobra.Command, args []string) error {
 			squareInfo, err := squareFromCoord(mousePos)
 
 			if err == nil {
-				//fmt.Println(mousePos, " -> ", squareInfo)
-				if rl.IsMouseButtonDown(rl.MouseLeftButton) {
-					squareInfo.Color = rl.Red
+				if rl.IsMouseButtonDown(rl.MouseRightButton) {
+					squareInfo.Color = rl.Blank
 					drawnContents[squareInfo.GridCord] = squareInfo
-				} else {
-					squareInfo.Color = rl.Blue
+				} else if rl.IsMouseButtonDown(rl.MouseLeftButton) {
+					squareInfo.Color = drawColor
 					traceContents[squareInfo.GridCord] = squareInfo
 				}
 			}
 		}
+
 		rl.EndDrawing()
 	}
 
@@ -152,25 +183,69 @@ func paint(cmd *cobra.Command, args []string) error {
 }
 
 func drawGrid() {
-	for r := int32(0); r <= windowHeight; r += spacing {
-		rl.DrawLine(0, r, windowWidth, r, rl.Black)
+	// draw row lines
+	for rowNum, rowBegin := int32(0), gridOrigin; rowNum <= numRows; rowNum++ {
+		rowEnd := rl.NewVector2(rowBegin.X+float32(gridWidth), rowBegin.Y)
+		rl.DrawLineEx(rowBegin, rowEnd, 1.0, gridColor)
+		rowBegin.Y += float32(spacing)
 	}
-	for c := int32(0); c <= windowWidth; c += spacing {
-		rl.DrawLine(c, 0, c, windowHeight, rl.Black)
+	// draw column lines
+	for colNum, colBegin := int32(0), gridOrigin; colNum <= numColumns; colNum++ {
+		colEnd := rl.NewVector2(colBegin.X, colBegin.Y+float32(gridHeight))
+		rl.DrawLineEx(colBegin, colEnd, 1.0, gridColor)
+		colBegin.X += float32(spacing)
 	}
+}
+
+func drawColorInputs(position rl.Vector2, red, green, blue *int) (rl.Color, rl.Vector2) {
+	position.X += 5
+	drawColorInput("red", red, position)
+	position.Y += 45
+	drawColorInput("green", green, position)
+	position.Y += 45
+	drawColorInput("blue", blue, position)
+	position.Y += 45
+
+	color := makeColor(*red, *green, *blue, 255)
+	rl.DrawRectangleV(position, rl.NewVector2(spacingFloat, spacingFloat), color)
+	return color, rl.NewVector2(position.X, position.Y+spacingFloat)
+}
+
+func drawColorInput(name string, colorValue *int, position rl.Vector2) {
+	rg.Label(rl.NewRectangle(position.X, position.Y, 50, 20), name)
+	color := rg.TextBox(rl.NewRectangle(position.X, position.Y+20, 50, 20), strconv.Itoa(*colorValue))
+	*colorValue, _ = strconv.Atoi(color)
+	if *colorValue > maxRGB {
+		*colorValue = maxRGB
+	}
+}
+
+func makeColor(red, green, blue, alpha int) rl.Color {
+	return rl.NewColor(uint8(red), uint8(green), uint8(blue), uint8(alpha))
+}
+
+func drawDecaySettings(position rl.Vector2, decayValue *bool) (bool, rl.Vector2) {
+	rg.Label(rl.NewRectangle(position.X, position.Y, 50, 20), "Decay")
+	position.Y += 20
+	*decayValue = rg.CheckBox(rl.NewRectangle(position.X, position.Y, 50, 20), *decayValue)
+	return *decayValue, position
 }
 
 func drawSquares(squareContets map[GridCord]SquareInfo, fadeMode bool) {
 	for cord, square := range squareContets {
-		if timeLeft := time.Now().Sub(square.CreatedAt); timeLeft < decayTime {
-			// scale for alpha
-			var alpha = float32(1.0)
-			if fadeMode {
-				alpha = 1.0 - float32(timeLeft.Nanoseconds())/float32(decayTime.Nanoseconds())
+		if decayMode {
+			if timeLeft := time.Now().Sub(square.CreatedAt); timeLeft < decayTime {
+				// scale for alpha
+				var alpha = float32(1.0)
+				if fadeMode {
+					alpha = 1.0 - float32(timeLeft.Nanoseconds())/float32(decayTime.Nanoseconds())
+				}
+				rl.DrawRectangleV(square.Origin, rl.NewVector2(spacingFloat, spacingFloat), rl.Fade(square.Color, alpha))
+			} else {
+				delete(squareContets, cord)
 			}
-			rl.DrawRectangleV(square.Vector2, rl.NewVector2(spacingFloat, spacingFloat), rl.Fade(square.Color, alpha))
 		} else {
-			delete(squareContets, cord)
+			rl.DrawRectangleV(square.Origin, rl.NewVector2(spacingFloat, spacingFloat), square.Color)
 		}
 	}
 }
@@ -208,7 +283,7 @@ func squareFromCoord(vec rl.Vector2) (SquareInfo, error) { // return top left of
 	y := int32(vec.Y)
 
 	if x <= 0 || y <= 0 ||
-		x >= windowWidth || y >= windowHeight {
+		x >= gridWidth || y >= gridHeight {
 		return SquareInfo{}, errors.New("Outside of screen bounds")
 	}
 	xPos := x / spacing
@@ -216,7 +291,7 @@ func squareFromCoord(vec rl.Vector2) (SquareInfo, error) { // return top left of
 
 	info := SquareInfo{
 		GridCord:  GridCord{uint8(xPos), uint8(yPos)},
-		Vector2:   rl.NewVector2(float32(xPos*spacing), float32(yPos*spacing)),
+		Origin:    rl.NewVector2(float32(xPos*spacing), float32(yPos*spacing)),
 		CreatedAt: time.Now(),
 	}
 	return info, nil
